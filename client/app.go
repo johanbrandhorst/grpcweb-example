58551de4c65ad4f7bf7c8836b21502cdfc08e7ce
Johan Brandhorst <johan@cognitivelogic.com>
2017-06-19 23:04:51 +0100

    Minify output JS

--- client/app.go
+++ client/app.go
@@ -7,7 +7,7 @@ import (
 	"github.com/johanbrandhorst/grpcweb-example/client/container"
 )
 
-//go:generate gopherjs build app.go -o html/client.js
+//go:generate gopherjs build app.go -m -o html/client.js
 //go:generate go-bindata -pkg compiled -nometadata -o compiled/client.go -prefix html ./html
 //go:generate bash -c "rm html/*.js*"
 
