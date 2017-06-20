09f2154d2afd564a927627282bdaa5644af68d7b
Johan Brandhorst <johan@cognitivelogic.com>
2017-06-20 08:01:06 +0100

    Loosen TLS requirements for greater compatibility

--- main.go
+++ main.go
@@ -102,15 +102,6 @@ func main() {
 				tls.CurveP256,
 				tls.X25519,
 			},
-			MinVersion: tls.VersionTLS12,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			},
 		},
 		Handler: hstsHandler(mux.ServeHTTP),
 	}
