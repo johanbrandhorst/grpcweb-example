f2dadd19533cb34a576884456bcaa3b5da2a1d97
Johan Brandhorst <johan@cognitivelogic.com>
2017-08-14 21:43:23 +0100

    Replace protogen with a Makefile

--- protogen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2017 Johan Brandhorst. All Rights Reserved.
-# See LICENSE for licensing terms.
-
-# Generate protofiles
-protoc proto/library/book_service.proto \
-    --gopherjs_out=plugins=grpc,Mgoogle/protobuf/timestamp.proto=github.com/johanbrandhorst/protobuf/ptypes/timestamp:./client/ \
-    --go_out=plugins=grpc:./server/
-
